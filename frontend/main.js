72d7c40096a4890230b1863cd50ed3b8656ceb2c
binellijj <binellijj@gmail.com>
2024-02-08 15:49:34 -0500

    editing frontend

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = "https://getresumecounterjohn.azurewebsites.net/api/GetResumeCounter?code=-fHwvAgMlBOjvwwzndBf8MsjnhuzaH8hWoE5dFPLXMKjAzFukUWndg=="
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
